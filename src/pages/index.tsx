8b620e556305929de3a6713bd225a08c1cf70fa7
rai <mj82xx@gmail.com>
2023-02-16 00:24:23 +0900

    Heddingの余白と、listの余白を調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,48 +21,50 @@ export default function Home() {
         <Container centerContent>
           <Stack spacing={8}>
             <Box>
-              <Heading as='h1'>Rai</Heading>
+              <Heading as='h1' pb={2}>
+                Rai
+              </Heading>
               <Text as='p'>
                 I&apos;m a student at the Tokyo University of Science, majoring in Information Science.
               </Text>
             </Box>
             <Box>
-              <Heading as='h2'>Programming</Heading>
-              <Container>
-                <UnorderedList>
-                  <ListItem>Python</ListItem>
-                  <ListItem>Rust</ListItem>
-                  <ListItem>Typescript</ListItem>
-                </UnorderedList>
-              </Container>
+              <Heading as='h2' pb={2}>
+                Programming
+              </Heading>
+              <UnorderedList pl={2}>
+                <ListItem>Python</ListItem>
+                <ListItem>Rust</ListItem>
+                <ListItem>Typescript</ListItem>
+              </UnorderedList>
             </Box>
             <Box>
-              <Heading as='h2'>Interests</Heading>
-              <Container>
-                <UnorderedList>
-                  <ListItem>Tauri</ListItem>
-                  <ListItem>WebAssembly</ListItem>
-                  <ListItem>Compiler</ListItem>
-                  <ListItem>3DCG</ListItem>
-                </UnorderedList>
-              </Container>
+              <Heading as='h2' pb={2}>
+                Interests
+              </Heading>
+              <UnorderedList pl={2}>
+                <ListItem>Tauri</ListItem>
+                <ListItem>WebAssembly</ListItem>
+                <ListItem>Compiler</ListItem>
+                <ListItem>3DCG</ListItem>
+              </UnorderedList>
             </Box>
             <Box>
-              <Heading as='h2'>Links</Heading>
-              <Container>
-                <UnorderedList>
-                  <ListItem>
-                    <Link as={NextLink} href='https://github.com/r4ai'>
-                      GitHub
-                    </Link>
-                  </ListItem>
-                  <ListItem>
-                    <Link as={NextLink} href='https://zenn.dev/t4aru'>
-                      Zenn
-                    </Link>
-                  </ListItem>
-                </UnorderedList>
-              </Container>
+              <Heading as='h2' pb={2}>
+                Links
+              </Heading>
+              <UnorderedList pl={2}>
+                <ListItem>
+                  <Link as={NextLink} href='https://github.com/r4ai'>
+                    GitHub
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link as={NextLink} href='https://zenn.dev/t4aru'>
+                    Zenn
+                  </Link>
+                </ListItem>
+              </UnorderedList>
             </Box>
           </Stack>
         </Container>
